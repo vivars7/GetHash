78f471a42940a33d27436c841798be23a02bed2e
vivars7 <spacemarket@naver.com>
2022-02-03 14:59:12 +0900

    change language.

--- hash.py
+++ hash.py
@@ -8,17 +8,17 @@ def getHash(input_file):
         f.close()
         return hashlib.sha256(data).hexdigest().upper()
     except Exception as e:
-        print("대상 파일이 잘못되었습니다.", e)
+        print("Target file is invalid.", e)
 
 def getHashs(args):
     del args[0]
     print("-----------------------------------------------------------------------------")
     for arg in args:
-        print("TARGET FILE PATH : " + arg)
+        print("File path : " + arg)
         print(getHash(str(arg)))
         print("-----------------------------------------------------------------------------")
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print("매개변수가 부족합니다. 사용방법 : hash [대상파일명] [대상파일명]...")
+        print("Invalid arguments. Usage : hash [target file path] [target file path]...")
     getHashs(sys.argv)
